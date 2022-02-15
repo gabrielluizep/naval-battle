bb5adca87e0d509696a1aa9413c449484289795f
gabrielluizep <gabrielluizep@hotmail.com>
2022-02-15 19:33:40 -0300

    :sparkles: Implemented shot function

--- naval-battle.c
+++ naval-battle.c
@@ -3,15 +3,15 @@
 #include <time.h>
 
 // Defining constants
-
 #define BATTLEFIELD_SIZE 10
 #define MAX_SHOTS 30
 #define MAX_NAME 30
-#define FREE_SPACE 0
 
+#define FREE_SPACE 0
 #define AIRCRAFT_CARRIER 1
 #define FRIGATE 2
 #define SUBMARINE 3
+#define BOMBED_SPACE 10
 
 // Global variables
 int battleField[BATTLEFIELD_SIZE][BATTLEFIELD_SIZE];
@@ -24,95 +24,152 @@ int randomY;
 // Helper functions
 
 void zeroMatrix() {
-  for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
-    for (int j = 0; j < BATTLEFIELD_SIZE; j++) {
-      battleField[i][j] = FREE_SPACE;
+    for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
+        for (int j = 0; j < BATTLEFIELD_SIZE; j++) {
+            battleField[i][j] = FREE_SPACE;
+        }
     }
-  }
 }
 
 void drawSpace() {
-  do {
-    randomX = rand() % BATTLEFIELD_SIZE;
-    randomY = rand() % BATTLEFIELD_SIZE;
-  } while (battleField[randomX][randomY] != FREE_SPACE);
+    do {
+        randomX = rand() % BATTLEFIELD_SIZE;
+        randomY = rand() % BATTLEFIELD_SIZE;
+    } while (battleField[randomX][randomY] != FREE_SPACE);
 }
 
 void setFrigates() {
-  for (int i = 0; i < 4; i++) {
-    drawSpace();
-    battleField[randomX][randomY] != FRIGATE;
-  }
+    for (int i = 0; i < 4; i++) {
+        drawSpace();
+        battleField[randomX][randomY] = FRIGATE;
+    }
 }
 
 void setSubmarines() {
-  for (int i = 0; i < 4; i++) {
-    drawSpace();
-    battleField[randomX][randomY] != SUBMARINE;
-  }
+    for (int i = 0; i < 4; i++) {
+        drawSpace();
+        battleField[randomX][randomY] = SUBMARINE;
+    }
 }
 
 void setAircraftCarrier() {
-  drawSpace();
-  battleField[randomX][randomY] != AIRCRAFT_CARRIER;
+    drawSpace();
+    battleField[randomX][randomY] = AIRCRAFT_CARRIER;
 }
 
 void setShips() {
-  srand(time(NULL));
+    srand(time(NULL));
 
-  setFrigates();
-  setSubmarines;
-  setAircraftCarrier();
+    setFrigates();
+    setSubmarines();
+    setAircraftCarrier();
 }
 
 // UI Drawing
 
 void drawLine(int line) {
-  printf("     ");
+    printf("     ");
 
-  for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
-    printf("| %2d ", battleField[line][i]);
-  }
+    for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
+        printf("| %2d ", battleField[line][i]);
+    }
 
-  printf("|/n");
-  printf("     +----+----+----+----+----+----+----+----+----+----+\n");
+    printf("|/n");
+    printf("     +----+----+----+----+----+----+----+----+----+----+\n");
 }
 
 void drawHiddenLine(int line) {
-  printf("     ");
-  for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
-    if (battleField[line][i] >= 0 && battleField[line][i] <= 3)
-      printf("| -- ");
-    else
-      printf("| %2d ", battleField[line][i]);
-  }
-  printf("|\n");
-  printf("     +----+----+----+----+----+----+----+----+----+----+\n");
+    printf("     ");
+    for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
+        if (battleField[line][i] >= 0 && battleField[line][i] <= 3)
+            printf("| -- ");
+        else
+            printf("| %2d ", battleField[line][i]);
+    }
+    printf("|\n");
+    printf("     +----+----+----+----+----+----+----+----+----+----+\n");
 }
 
 void drawHiddenMatrix() {
-  printf("     +----+----+----+----+----+----+----+----+----+----+\n");
-  for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
-    drawHiddenLine(i);
-  }
+    printf("     +----+----+----+----+----+----+----+----+----+----+\n");
+    for (int i = 0; i < BATTLEFIELD_SIZE; i++) {
+        drawHiddenLine(i);
+    }
 }
 
+struct coordinates {
+    int x;
+    int y;
+};
+
 int shot() {
-  // Develop
+    if (shotCounter >= MAX_SHOTS) {
+        return 1;
+    }
+
+    drawHiddenMatrix();
+
+    struct coordinates shot;
+
+    do {
+        printf("Selecione qual coluna você deseja atirar\n");
+
+        scanf(" %d", &shot.x);
+
+    } while (shot.x < 0 || shot.x > 9);
+
+    do {
+        printf("Selecione qual linha você deseja atirar\n");
+
+        scanf(" %d", &shot.y);
+
+    } while (shot.y < 0 || shot.y > 9);
+
+    shotCounter++;
+
+    printf("tiro acertou %d\n", battleField[shot.x][shot.y]);
+
+    switch (battleField[shot.x][shot.y]) {
+        case FREE_SPACE:
+            printf("Você atirou, porém não havia nada naquela posição. :(\n");
+            break;
+        case AIRCRAFT_CARRIER:
+            printf("Parabéns, você acertou um porta-aviões! :D\n");
+            score += 20;
+            break;
+        case FRIGATE:
+            printf("Parabéns, você acertou uma fragata! :)\n");
+            score += 10;
+            break;
+        case SUBMARINE:
+            printf("Parabéns, você acertou um submarino! XD\n");
+            score += 30;
+            break;
+
+        default:
+            return 0;
+            break;
+    }
+
+    battleField[shot.x][shot.y] += BOMBED_SPACE;
+
+    return 0;
 }
 
 int main() {
-  int state;
+    system("clear");
 
-  zeroMatrix();
-  setShips();
-  drawHiddenMatrix();
+    int state;
 
-  do {
-    state = shot();
-  } while (state == 0);
+    zeroMatrix();
+    setShips();
 
-  // Treat state
+    do {
+        state = shot();
+        system("clear");
+    } while (state == 0);
 
-  return 0;
+    printf("Seu placar foi de %d\n", score);
+
+    return 0;
 }
